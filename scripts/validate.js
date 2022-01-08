fdd43c10716d2fdb5c1c02b87c35b5efe9147668
Valya <vshulya@gmail.com>
2022-01-08 15:30:34 -0800

    const isValid

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,17 +1,37 @@
-// enableValidation({
-//   formSelector: '.pop-up__form',
-//   inputSelector: '.pop-up__input',
-//   submitButtonSelector: '.pop-up__button',
-//   inactiveButtonClass: 'pop-up__button_disabled',
-//   inputErrorClass: 'pop-up__input_type_error',
-//   errorClass: 'pop-up__error_visible'
-// }); 
+const isValid = (form, input) => {
+  if (!input.validity.valid) {
+    console.log('Invalid');
+    // showInputError(form, input, input.validationMessage);
+  } else {
+    console.log('Valid');
+    // hideInputError(form, input);
+  }
+};
 
-// const formSelector = document.querySelectorAll('pop-up__form');
+const setInputList = (form, { inputSelector }) => {
+  const inputs = form.querySelectorAll(inputSelector);
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      isValid(form, input);
+    })
+  })
+}
 
-// const inputSelector = () => {
-//   formSelector.querySelectorAll('pop-up__input');
-//   inputSelector.addEventListener('input', function (evt) {
-//     console.log(evt.target.validity.valid);
-//   });
-// };
+const enableValidation = ({ formSelector, ...rest }) => {
+  const forms = document.querySelectorAll(formSelector);
+  forms.forEach((form) => {
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setInputList(form, rest);
+  });
+};
+
+enableValidation({
+  formSelector: '.pop-up__form',
+  inputSelector: '.pop-up__input',
+  submitButtonSelector: '.pop-up__button',
+  inactiveButtonClass: 'pop-up__button_disabled',
+  inputErrorClass: 'pop-up__input_type_error',
+  errorClass: 'pop-up__error_visible'
+});
