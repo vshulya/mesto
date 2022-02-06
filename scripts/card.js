bcdc42a4a7459d4cf54b54270688a6e2967530a9
Valya <vshulya@gmail.com>
2022-02-06 19:38:55 +0300

    feat: modul card w/o esc

---
+++ scripts/card.js
@@ -0,0 +1,126 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Class CARD
+const photoModal = document.querySelector('.pop-up_place_fullsize-photo');
+const closePhotoModalButton = photoModal.querySelector('.pop-up__close');
+const fullSizePhoto = document.querySelector('.pop-up__fullsize-photo');
+const fullSizePhotoTitle = document.querySelector('.pop-up__figcaption');
+
+class Card {
+  constructor(data, cardSelector) {
+    this._link = data.link;
+    this._name = data.name;
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate() {
+    // забираем разметку из HTML и клонируем элемент
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+
+    // вернём DOM-элемент карточки
+    return cardElement;
+  }
+
+  generateCard() {
+    // Запишем разметку в приватное поле _element. 
+    // Так у других элементов появится доступ к ней.
+    this._element = this._getTemplate();
+    this._setEventListeners();
+
+    // Добавим данные
+    this._element.querySelector('.card__image').src = this._link;
+    this._element.querySelector('.card__text').textContent = this._name;
+
+    // Вернём элемент наружу
+    return this._element;
+  }
+
+
+  _handleOpenPopup() {
+    fullSizePhoto.src = this._link;
+    fullSizePhotoTitle.textContent = this._name;
+    photoModal.classList.add('pop-up_opened');
+  }
+
+  _handleClosePopup() {
+    fullSizePhoto.src = '';
+    photoModal.classList.remove('pop-up_opened');
+  }
+
+  _handleDeleteIcone() {
+    this._element.remove();
+  }
+
+  _handleLikeClick() {
+    this._element.querySelector('.card__like').classList.toggle('card__like_active');
+  };
+
+
+  _setEventListeners() {
+    this._element.querySelector('.card__like').addEventListener('click', () => {
+      this._handleLikeClick();
+    });
+
+    this._element.querySelector('.card__image').addEventListener('click', () => {
+      this._handleOpenPopup();
+    });
+
+    closePhotoModalButton.addEventListener('click', () => {
+      this._handleClosePopup();
+    });
+
+    this._element.querySelector('.card__delete-button').addEventListener('click', () => {
+      this._handleDeleteIcone();
+    });
+
+
+    //TODO BY ESC
+    // this._element.addEventListener('keydown', (event) => {
+    //   if (event.key === 'Escape') {
+    //     this._handleClosePopup();
+    //   };
+    // });
+  };
+};
+
+function renderCard(item) {
+  const card = new Card(item, '.card-template');
+  const cardElement = card.generateCard();
+  document.querySelector('.cards-list').append(cardElement);
+};
+
+
+initialCards.forEach((item) => {
+  renderCard(item);
+});
+
+export { initialCards, photoModal, closePhotoModalButton, fullSizePhoto, fullSizePhotoTitle, Card, renderCard };
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
 //modal
 const editModal = document.querySelector('.pop-up_place_title');
 const cardModal = document.querySelector('.pop-up_place_card');
-const photoModal = document.querySelector('.pop-up_place_fullsize-photo');
-
 
 //button
 const openEditModalButton = document.querySelector('.profile__edit-button');
 const closeEditModalButton = editModal.querySelector('.pop-up__close');
 const openCardModalButton = document.querySelector('.profile__add-button');
 const closeCardModalButton = cardModal.querySelector('.pop-up__close');
-const closePhotoModalButton = photoModal.querySelector('.pop-up__close');
+
 
 //input
 const nameInput = document.querySelector('.pop-up__input_type_name');
@@ -87,8 +85,6 @@ closeCardModalButton.addEventListener('click', () => {
 
 const formElementAddCard = cardModal.querySelector('.pop-up__form');
 
-closePhotoModalButton.addEventListener('click', () => closeModal(photoModal));
-
 //close modal on overlay
 
 const wireCloseModalOverlay = () => {
@@ -110,64 +106,6 @@ function closeByEscape(evt) {
   };
 };
 
-
-//card-template
-
-const cardList = document.querySelector('.cards-list');
-const template = document.querySelector('.card-template').content;
-
-const fullSizePhoto = document.querySelector('.pop-up__fullsize-photo');
-const fullSizePhotoTitle = document.querySelector('.pop-up__figcaption');
-
-function deleteHandler(evt) {
-  evt.target.parentElement.remove();
-}
-
-function toggleLike(evt) {
-  evt.target.classList.toggle('card__like_active');
-};
-
-function handlePreviewPicture() {
-  openModal(photoModal);
-};
-
-function createCard(cardData) {
-  const element = template.cloneNode(true);
-  const cardImage = element.querySelector('.card__image');
-  const cardTitle = element.querySelector('.card__text');
-  const deleteButton = element.querySelector('.card__delete-button');
-  const likeButton = element.querySelector('.card__like');
-  const openPhotoModalButton = cardImage;
-
-  cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
-  cardTitle.textContent = cardData.name;
-
-
-  openPhotoModalButton.addEventListener('click', function () {
-    handlePreviewPicture();
-    fullSizePhoto.src = cardData.link;
-    fullSizePhoto.alt = cardData.name;
-    fullSizePhotoTitle.textContent = cardData.name;
-  });
-
-  deleteButton.addEventListener('click', deleteHandler);
-  likeButton.addEventListener('click', toggleLike);
-
-  return element;
-};
-
-function renderCard(cardData) {
-  const newCard = createCard(cardData);
-  cardList.prepend(newCard);
-};
-
-wireCloseModalOverlay();
-
-initialCards.forEach(cardData => {
-  renderCard(cardData)
-});
-
 formElementAddCard.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
@@ -181,3 +119,6 @@ formElementAddCard.addEventListener('submit', (evt) => {
 
 });
 
+wireCloseModalOverlay();
+
+import { initialCards, photoModal, closePhotoModalButton, fullSizePhoto, fullSizePhotoTitle, Card, renderCard } from './card.js';
